4fba0c0bc113bbdae56fe9d08947377b25cdedf8
mamoocha <koval.aleks94@gmail.com>
2025-06-06 03:53:01 +0300

    Update removezhop.js

--- octoprint_removezhop/static/js/removezhop.js
+++ octoprint_removezhop/static/js/removezhop.js
@@ -2,14 +2,28 @@ $(function() {
     function RemoveZHopViewModel(parameters) {
         var self = this;
 
-        self.settingsViewModel = parameters[0];
-        self.enabled = self.settingsViewModel.settings.plugins.removezhop.enabled;
-        self.z_threshold = self.settingsViewModel.settings.plugins.removezhop.z_threshold;
+        self.settings = parameters[0];
+
+        self.enabled = ko.observable(self.settings.settings.plugins.removezhop.enabled());
+        self.z_threshold = ko.observable(self.settings.settings.plugins.removezhop.z_threshold());
+
+        self.enabled.subscribe(function(newVal) {
+            self.settings.settings.plugins.removezhop.enabled(newVal);
+            self.settings.saveData();
+        });
+
+        self.z_threshold.subscribe(function(newVal) {
+            var val = parseFloat(newVal);
+            if (!isNaN(val)) {
+                self.settings.settings.plugins.removezhop.z_threshold(val);
+                self.settings.saveData();
+            }
+        });
     }
 
     OCTOPRINT_VIEWMODELS.push({
         construct: RemoveZHopViewModel,
         dependencies: ["settingsViewModel"],
-        elements: ["#sidebar_plugin_removezhop"]
+        elements: [".sidebar_plugin_removezhop"]
     });
 });
